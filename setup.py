904503a735bcc0a28369fa0a7fc71279d8ceff65
Laurent Arnoud <laurent.arnoud@platform.sh>
2023-10-10 11:39:09 +0200

    Force latest version instead of unknown

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 try:
     __version__ = version('glustercli')
 except PackageNotFoundError:
-    __version__ = "unknown"
+    __version__ = "0.8.6"
 
 setup(
     name='glustercli',
